3f7e43dbb26b8acd4312d4fb83c2f6a17b24cf59
mathv96 <matheus.victor@ymail.com>
2018-09-22 18:56:58 -0300

    Update: finish the main structure.

--- project/main.py
+++ project/main.py
@@ -1,15 +1,29 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class Insert_event(Resource):
+#def to the execution of each route
+class Index(Resource):
     def get(self):
-        #insert
-        return {'teste':'testando'}, 200
+        response = jsonify({'result':'API is working.'})
+        response.status_code = 200
+        return response
 
-api.add_resource(Insert_event, '/')
+class Insert_event(Resource):
+    def post(self):
+        #insert
+        inputed_json = request.get_json()
+        #dao will be called
+        response = jsonify({'result':'Event saved.'})
+        response.status_code = 201
+        return response
+
+
+#routes
+api.add_resource(Index, '/')
+api.add_resource(Insert_event, '/insert_event')
 
 if __name__== '__main__':
     app.run(debug=True)
