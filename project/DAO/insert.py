72a583a04fd10bc14fab9751ce426331edf7dd4d
mathv96 <matheus.victor@ymail.com>
2018-09-23 18:00:21 -0300

    Update: fix the code to works with the new library.

--- project/DAO/insert.py
+++ project/DAO/insert.py
@@ -1,14 +1,16 @@
-import conn
+import DAO.conn as connection
+
+from config import * 
 
 def insert_event(event):
-    conn = conn.connection_mysql()
+    conn = connection.connection_mysql()
     query = "INSERT INTO {} VALUES('{}', '{}', {}, '{}', '{}')".format(
             table_name, event['component'], event['version'], event['responsible', event['status']]
         )
     if conn:
         cursor = conn.cursor()
-            try:
-                cursor.execute(query)
-            except Exception as e:
-                return False
-            return True
+        try:
+            cursor.execute(query)
+        except Exception as e:
+            return False
+        return True
