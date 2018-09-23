eeab8612d21924fc658bf120e31689001ee2e7f7
mathv96 <matheus.victor@ymail.com>
2018-09-23 19:29:53 -0300

    Update: fix the query to insert the events and add the configuration to get the date.

--- project/DAO/insert.py
+++ project/DAO/insert.py
@@ -1,16 +1,22 @@
 import DAO.conn as connection
+from datetime import datetime
 
 from config import * 
 
 def insert_event(event):
     conn = connection.connection_mysql()
-    query = "INSERT INTO {} VALUES('{}', '{}', {}, '{}', '{}')".format(
-            table_name, event['component'], event['version'], event['responsible', event['status']]
+    query = "INSERT INTO {} (component, version, responsible, status, date) VALUES('{}', '{}', '{}', '{}', '{}')".format(
+            table_name, event['component'], event['version'], event['responsible'], event['status'], datetime.now()
         )
     if conn:
         cursor = conn.cursor()
         try:
             cursor.execute(query)
+            conn.commit()
+            conn.close()
         except Exception as e:
+            conn.rollback()
+            conn.close()
+            print(e)
             return False
         return True
