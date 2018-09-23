04c39e54ee7843c72f034d45be788bac6b905019
mathv96 <matheus.victor@ymail.com>
2018-09-23 19:28:51 -0300

    Update: fix the table creation with primarykey and auto increment.

--- project/DAO/create_table.py
+++ project/DAO/create_table.py
@@ -4,12 +4,13 @@ from config import *
 
 def create_table():
     conn = connection.connection_mysql()
-    query = "CREATE TABLE IF NOT EXISTS {} ({} VARCHAR (200) NOT NULL,{} VARCHAR (20) NOT NULL,{} VARCHAR (200) NOT NULL,{} VARCHAR (20) NOT NULL, {} DATETIME NOT NULL)".format(
+    query = "CREATE TABLE IF NOT EXISTS {} (id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, {} VARCHAR (200) NOT NULL,{} VARCHAR (20) NOT NULL,{} VARCHAR (200) NOT NULL,{} VARCHAR (20) NOT NULL, {} DATETIME NOT NULL)".format(
             table_name, 'component', 'version', 'responsible', 'status', 'date')
     if conn:
         cursor = conn.cursor()
         try:
             cursor.execute(query)
+            conn.close()
         except Exception as e:
             return False
         return True
