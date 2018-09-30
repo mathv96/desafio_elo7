834c25f20b9276e73435f875e9a873bfbf51f2c9
mathv96 <matheus.victor@ymail.com>
2018-09-30 18:57:24 -0300

    Update: Improved the code to return the information as json.

--- project/DAO/list.py
+++ project/DAO/list.py
@@ -1,24 +1,27 @@
-import DAO.conn as connection
-from datetime import datetime
+import json
 
+import DAO.conn as connection
 from config import * 
 
-def lis_events():
+def list_events():
     conn = connection.connection_mysql() 
     query = "SELECT * FROM {}".format(table_name)
 
     if conn:
         cursor = conn.cursor()
+        json_result = None
         try:
             cursor.execute(query)
             result_list = [list(line) for line in cursor.fetchall()]
+            result_dict={}
+            result_dict['result']=[]
             for line in result_list:
-                print(line)
+                result_dict['result'].append({"id": str(line[0]), "component": str(line[1]), "responsible": str(line[3]), "status": str(line[4]), "date": str(line[5])})
+            json_result = result_dict
             conn.close()
         except Exception as e:
             conn.close()
-            print(e.__context__)
             return False
-        return True
+        return json_result
     else:
         return False
