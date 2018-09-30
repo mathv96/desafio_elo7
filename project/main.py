de2145c189f5a97fbb1bd03dff5af09f798550cf
mathv96 <matheus.victor@ymail.com>
2018-09-30 02:45:54 -0300

    Upate: changes to work with the new features.

--- project/main.py
+++ project/main.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 
 from validation.validate_event import validate_event
 from DAO.insert import insert_event
+from DAO.list import lis_events
 from DAO.create_table import create_table
 
 
@@ -20,26 +21,39 @@ class Insert_event(Resource):
     def post(self):
         inputed_json = request.get_json()
         response_validation = validate_event(inputed_json)
-        print(response_validation)
         if response_validation == False:
             response = jsonify(result="Please, send a request with the right json structure.")
             response.status_code = 200
             return response
         else:    
-            response_creatiion = create_table()
-            response_insert = insert_event(inputed_json)
-            if response_insert:
-                response = jsonify({'result':'Event saved.'})
-                response.status_code = 201
-                return response
+            response_creation = create_table()
+            if response_creation:
+                response_insert = insert_event(inputed_json)
+                if response_insert:
+                    response = jsonify({'result':'Event saved.'})
+                    response.status_code = 201
+                    return response
+                else:
+                    response = jsonify({'result':'Event was not saved.'})
+                    response.status_code = 200
+                    return response    
             else:
-                response = jsonify({'result':'Event was not saved.'})
+                response = jsonify({'result':'Was not possible to create table, verify if the database is avaible.'})
                 response.status_code = 200
                 return response    
+
+
+class List_events(Resource):
+    def get(self):
+        lis_events()
+        return "Events listed."
+
+
 
 #routes
 api.add_resource(Index, '/')
 api.add_resource(Insert_event, '/insert_event')
+api.add_resource(List_events, '/list_events')
 
 if __name__== '__main__':
     app.run(debug=True)
