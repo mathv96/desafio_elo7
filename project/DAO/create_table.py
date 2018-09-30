b3a35b68dd54d5b05906aab084532828d909ca94
mathv96 <matheus.victor@ymail.com>
2018-09-30 02:44:06 -0300

    Fix: add conditions to catch a new error.

--- project/DAO/create_table.py
+++ project/DAO/create_table.py
@@ -3,7 +3,7 @@ import DAO.conn as connection
 from config import * 
 
 def create_table():
-    conn = connection.connection_mysql()
+    conn = connection.connection_mysql() 
     query = "CREATE TABLE IF NOT EXISTS {} (id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, {} VARCHAR (200) NOT NULL,{} VARCHAR (20) NOT NULL,{} VARCHAR (200) NOT NULL,{} VARCHAR (20) NOT NULL, {} DATETIME NOT NULL)".format(
             table_name, 'component', 'version', 'responsible', 'status', 'date')
     if conn:
@@ -12,5 +12,9 @@ def create_table():
             cursor.execute(query)
             conn.close()
         except Exception as e:
+            conn.close()
+            print(e.__context__)
             return False
         return True
+    else:
+        return False
