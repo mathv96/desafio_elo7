0691200a07d306e2fcb5ce83b52359418001fc25
mathv96 <matheus.victor@ymail.com>
2018-09-30 02:44:19 -0300

    Fix: add conditions to catch a new error.

--- project/DAO/insert.py
+++ project/DAO/insert.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from config import * 
 
 def insert_event(event):
-    conn = connection.connection_mysql()
+    conn = connection.connection_mysql() 
     query = "INSERT INTO {} (component, version, responsible, status, date) VALUES('{}', '{}', '{}', '{}', '{}')".format(
             table_name, event['component'], event['version'], event['responsible'], event['status'], datetime.now()
         )
@@ -17,6 +17,8 @@ def insert_event(event):
         except Exception as e:
             conn.rollback()
             conn.close()
-            print(e)
+            print(e.__context__)
             return False
         return True
+    else:
+        return False
