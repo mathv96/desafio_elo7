e67f864fee30cd461cbffa9fde468a5eadb22f9c
mathv96 <matheus.victor@ymail.com>
2018-09-30 02:43:04 -0300

    Update: some changes to catch some errors and print they on console.

--- project/DAO/conn.py
+++ project/DAO/conn.py
@@ -1,12 +1,18 @@
 import pymysql
 
-from config import * 
+from config import *
 
 def connection_mysql():
     conn = None
     try:
         conn = pymysql.connect(user=user_mysql, passwd=password_mysql, host=host, port=3306, db=database)
-    except Exception as e:
-        print (e)
+    except ConnectionRefusedError as e:
         conn = None
-    return conn
+        print(e.__context__)
+    except pymysql.err.OperationalError as e:
+        conn = None
+        print(e.__context__)
+    except Exception as e:
+        conn = None
+        print(e.__context__)
+    return conn 
